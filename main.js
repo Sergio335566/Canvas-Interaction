53aef64d764f242b3f9f6acf57eb86076276afdf
Serge Bocancea <bocanceaserge335566@gmail.com>
2019-01-20 21:22:59 +0100

    Randomize

--- main.js
+++ main.js
@@ -4,28 +4,49 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
-var x = Math.random() * window.innerWidth;
-var y = Math.random() * window.innerHeight;
-var dx = (Math.random() - 0.8) * 10;
-var dy = (Math.random() - 0.8) * 10;
-var size = 50;
+
+
+function Rect(x, y, dx, dy, size){
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.size = size;
+  this.draw = function(){
+    c.beginPath();
+    c.fillRect(this.x, this.y, this.size, this.size);
+    c.strokeStyle = 'red';
+    c.stroke();
+  }
+  this.update = function() {
+    if (this.x + size > innerWidth || this.x - this.size < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + size > innerHeight || this.y - this.size < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  }
+}
+
+// var x = Math.random() * window.innerWidth;
+// var y = Math.random() * window.innerHeight;
+// var dx = (Math.random() - 0.8) * 10;
+// var dy = (Math.random() - 0.8) * 10;
+// var size = 50;
+
+var rectangle = new Rect(200, 200, 4, 4, 30);
+
+
 
 function animate(){
   requestAnimationFrame(animate);
   // for (var i = 0; i < 50; i++) {
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.fillRect(x, y, size, size);
-    c.strokeStyle = 'red';
-    c.stroke();
-    if (x + size > innerWidth || x - size < 0) {
-      dx = -dx;
-    }
-    if (y + size > innerHeight || y - size < 0) {
-      dy = -dy;
-    }
-    x += dx;
-    y += dy;
+    rectangle.update();
+
   // }
 }
 animate();
