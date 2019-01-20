ce4cae9c94992f90667b1991f1f8579771469e71
Serge Bocancea <bocanceaserge335566@gmail.com>
2019-01-20 21:06:20 +0100

    add border collision

--- main.js
+++ main.js
@@ -4,11 +4,28 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
-for (var i = 0; i < 50; i++) {
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
-  c.beginPath();
-  c.fillRect(x, y, 100, 100);
-  c.strokeStyle = 'red';
-  c.stroke();
+var x = Math.random() * window.innerWidth;
+var y = Math.random() * window.innerHeight;
+var dx = (Math.random() - 0.8) * 10;
+var dy = (Math.random() - 0.8) * 10;
+var size = 50;
+
+function animate(){
+  requestAnimationFrame(animate);
+  // for (var i = 0; i < 50; i++) {
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.beginPath();
+    c.fillRect(x, y, size, size);
+    c.strokeStyle = 'red';
+    c.stroke();
+    if (x + size > innerWidth || x - size < 0) {
+      dx = -dx;
+    }
+    if (y + size > innerHeight || y - size < 0) {
+      dy = -dy;
+    }
+    x += dx;
+    y += dy;
+  // }
 }
+animate();
