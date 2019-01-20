c0de36e68d0274720318d5241f57d32a2ccbc4a4
Serge Bocancea <bocanceaserge335566@gmail.com>
2019-01-20 20:44:17 +0100

    random c position

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var rect = canvas.getContext('2d');
+var c = canvas.getContext('2d');
 
-rect.fillRect(100, 100, 100, 100);
-console.log(canvas)
+for (var i = 0; i < 50; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.fillRect(x, y, 100, 100);
+  c.strokeStyle = 'red';
+  c.stroke();
+}
